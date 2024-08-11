2f9025d7e2791b8d8c351dd8174aac8a3d843463
Christopher Peel <175068649+cpeel42@users.noreply.github.com>
2024-08-11 13:43:37 -0400

    Add fixed grid size, flex and random colors

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+const container = document.querySelector('.container');
+const gridResizer = document.querySelector('.gridResizer');
+
+gridResizer.addEventListener('click', (event) => {
+    width = prompt('enter new width');
+    height = prompt('enter new height');
+    if (height > 100 || width > 100) {
+        alert("The height or width must be less than 100") ;
+    } else {
+        removeGrid();
+        createGrid(height, width);
+    }
+})
+
+function removeGrid() {
+    container.querySelectorAll('.row').forEach(cell => {
+        cell.remove();
+    })
+};
+
+function createGrid(height, width) {
+    for (let i = 0; i < height; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+        container.appendChild(newRow);
+    
+        for (let j = 0; j < width; j++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('cell');
+            newCell.style.opacity = 1;
+            newCell.addEventListener('mouseover', (event) => {
+                newCell.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+                newCell.style.opacity -= 0.1;
+            })
+            newRow.appendChild(newCell);
+        } 
+    };
+}
+
